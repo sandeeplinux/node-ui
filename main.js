676a37596858de61bf25663aba6f4b379ae6d52d
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2016-07-12 12:33:30 -0700

    Updates

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var app = require('app');
 var BrowserWindow = require('browser-window');
 var globalShortcut = require('global-shortcut');
 var configuration = require('./configuration');
-var ipc = require('ipc');
 
 var mainWindow = null;
 var settingsWindow = null;
@@ -39,36 +38,3 @@ function setGlobalShortcuts() {
         mainWindow.webContents.send('global-shortcut', 1);
     });
 }
-
-ipc.on('close-main-window', function () {
-    app.quit();
-});
-
-ipc.on('open-settings-window', function () {
-    if (settingsWindow) {
-        return;
-    }
-
-    settingsWindow = new BrowserWindow({
-        frame: false,
-        height: 200,
-        resizable: false,
-        width: 200
-    });
-
-    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
-
-    settingsWindow.on('closed', function () {
-        settingsWindow = null;
-    });
-});
-
-ipc.on('close-settings-window', function () {
-    if (settingsWindow) {
-        settingsWindow.close();
-    }
-});
-
-ipc.on('set-global-shortcuts', function () {
-    setGlobalShortcuts();
-});
